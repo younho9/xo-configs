ee86808e020b9bcf0bd24402f0e9e28eb5847273
Younho Choo <yo+github@younho9.com>
2021-12-24 00:16:48 +0900

    fix: update `@typescript-eslint/naming-convention`
    fix variable naming convention

--- src/react.cjs
+++ src/react.cjs
@@ -36,14 +36,13 @@ module.exports = {
 					{
 						// Allowing StrictPascalCase for React components.
 						// see - https://github.com/xojs/eslint-config-xo-typescript/issues/48
-						selector: ['variable', 'function', 'objectLiteralProperty', 'objectLiteralMethod'],
+						selector: ['function', 'objectLiteralProperty', 'objectLiteralMethod'],
 						format: ['StrictPascalCase', 'strictCamelCase'],
 						leadingUnderscore: 'allowSingleOrDouble',
 						trailingUnderscore: 'allow',
 					},
 					{
 						selector: 'variable',
-						modifiers: ['const', 'global'],
 						format: ['UPPER_CASE', 'StrictPascalCase', 'strictCamelCase'],
 						leadingUnderscore: 'allowSingleOrDouble',
 						trailingUnderscore: 'allow',
