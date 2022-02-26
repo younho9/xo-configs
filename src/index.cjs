a1c355c66551235f22a9a17e4ae2ae1070c8c9c6
Younho Choo <yo+github@younho9.com>
2022-02-26 23:40:53 +0900

    feat: add `import/order` rule

--- src/index.cjs
+++ src/index.cjs
@@ -1,6 +1,16 @@
 /** @type {import('xo').Options & {overrides?: Array<{files: string} & import('xo').Options>}} */
 module.exports = {
 	rules: {
+		'import/order': [
+			'warn',
+			{
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+			},
+		],
 		/** @see https://github.com/sindresorhus/eslint-plugin-unicorn/issues/781 */
 		'unicorn/no-array-callback-reference': 'off',
 	},
